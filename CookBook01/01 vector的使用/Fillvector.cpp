3952c005bfdbd22ddc92a069c800b57802142cf7
logic96 <logmagic96@qq.com>
2019-06-09 16:23:11 +0800

    添加vector的简单实用

---
+++ CookBook01/01 vector的使用/Fillvector.cpp
@@ -0,0 +1,18 @@
+#include<vector>
+#include<fstream>
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+	vector<string>v;
+	ifstream in("Fillvector.cpp");
+	string line;
+	while (getline(in, line))
+		v.push_back(line);
+	for (int i = 0; i < v.size(); i++) {
+		cout << i << ": " << v[i] << endl;
+	}
+	system("pause");
+}
